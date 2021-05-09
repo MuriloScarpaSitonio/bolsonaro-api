5669f0b08c5fd9637639f43bcca05071e92383dd
MuriloScarpaSitonio <muriloscarpa@gmail.com>
2021-05-09 17:23:02 -0300

    aws eb multi container adjusts

--- django/scripts/set_env_vars.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from scripts.utils import yield_env_vars_from_file
-
-
-def set_env_vars(file_path: str) -> None:
-    for k, v in yield_env_vars_from_file(file_path=file_path):
-        os.environ[k] = v
---
+++ scripts/eb_create.py
@@ -0,0 +1,66 @@
+import os
+import subprocess
+import sys
+from typing import Iterable
+
+from utils import yield_env_vars_from_file
+
+
+def main(eb_env_name: str, env_files_path: Iterable[str]) -> None:
+    """Create an AWS Elastic Beanstalk environment with env vars.
+
+    Args:
+        eb_env_name (str): Environment's name;
+        env_files_path (Iterable[str]): Paths to env files.
+    """
+    eb_create_command = ["eb", "create", "--scale=1", "--envvars"]
+    aws_rds_command = ["--database.engine=postgres", "--database.instance=db.t2.micro"]
+    aws_eb_env_vars = []
+
+    for file_path in env_files_path:
+        failures = []
+        aws_rds_envs_command = []
+
+        if not os.path.exists(file_path):
+            print(f"ERROR!! {file_path} file is missing!")
+            return
+
+        for k, v in yield_env_vars_from_file(file_path=file_path):
+            if k == "DATABASE_USER":
+                aws_rds_envs_command.append(f"--database.username={v}")
+                continue
+            if k == "DATABASE_PASSWORD":
+                aws_rds_envs_command.append(f"--database.password={v}")
+                continue
+
+            if v:
+                aws_eb_env_vars.append(f"{k}={v}")
+            else:
+                failures.append(k)
+
+        if aws_rds_envs_command:
+            aws_rds_command = [*aws_rds_envs_command, *aws_rds_command]
+
+        if failures:
+            for failure in failures:
+                print(f"{failure} requires a value")
+
+    if len(aws_rds_command) == 4:
+        eb_create_command.insert(3, " ".join(aws_rds_command))
+    else:
+        print("ERROR!! DATABASE_USER or DATABASE_PASSWORD RDS env vars are missing!")
+        return
+
+    eb_create_command.append(",".join(aws_eb_env_vars))
+    print("Executing the following command:\n\n")
+    print(" ".join(eb_create_command))
+    print("\n\n")
+    subprocess.check_call(eb_create_command)
+
+
+if __name__ == "__main__":
+    pwd = os.environ.get("PWD")
+    if not pwd:
+        sys.exit("PWD env var is not available, please make sure to set it.")
+    FILES_PATH = [f"{pwd}/django/.env", f"{pwd}/react/.env", f"{pwd}/postgres/.env"]
+    main(eb_env_name="bolsonaro-api-env", env_files_path=FILES_PATH)
